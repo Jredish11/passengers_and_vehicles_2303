d637c0bf5e9b00a68275ce8335c0ab759a33af80
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-05 09:27:49 -0600

    feat: Add passengers method

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,11 +1,16 @@
 class Vehicle
-  attr_reader :year, :make, :model, :sped
+  attr_reader :year, 
+              :make, 
+              :model, 
+              :sped,
+              :passengers
   
   def initialize(year, make, model)
     @year = year
     @make = make
     @model = model
     @sped = false
+    @passengers = []
   end
 
   def speeding?
