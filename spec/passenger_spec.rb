c9432ff2557a26fcf5112e02261d3e4559300566
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-05 09:16:00 -0600

    test: Add test for adult?, driver?, drive methods

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -12,9 +12,23 @@ RSpec.describe Passenger do
       expect(taylor).to be_instance_of(Passenger)
       expect(taylor.name).to eq("Taylor")
       expect(taylor.age).to eq(12)
+    end
 
+    it "checks if passenger is an adult" do
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})  
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
 
+      expect(charlie.adult?).to eq(true)
+      expect(taylor.adult?).to eq(false)
+    end
 
+    it "checks if passenger is a driver" do
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})  
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      expect(charlie.driver?).to eq(false)
+      charlie.drive
+      expect(charlie.driver?).to eq(true)
     end
   end
 end
@@ -23,24 +37,3 @@ end
 
 
 
-
-
-
-
-
-# pry(main)> charlie.age
-# # => 18
-
-# pry(main)> charlie.adult?
-# # => true
-
-# pry(main)> taylor.adult?
-# # => false
-
-# pry(main)> charlie.driver?
-# # => false
-
-# pry(main)> charlie.drive
-
-# pry(main)> charlie.driver?
-# # => true
