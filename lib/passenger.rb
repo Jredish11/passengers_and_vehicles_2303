d4667c79ac72ae24bf7a523681a450cbb8b8d644
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-05 09:16:17 -0600

    feat: Add adult?, driver?, drive methods

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,7 +1,24 @@
 class Passenger
-    attr_reader :name, :age
+    attr_reader :name, :age, :driver
   def initialize(data)
     @name = data["name"]
     @age = data["age"]
+    @driver = false
+  end
+
+  def adult?
+    if @age >= 18
+      true
+    else
+      false
+    end
+  end
+
+  def driver?
+    @driver
+  end
+  
+  def drive
+    @driver = true
   end
 end
