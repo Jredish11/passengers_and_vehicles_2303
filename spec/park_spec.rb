01fc48eb05404772b52f988810eae66463655d15
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-05 10:43:31 -0600

    test: Add minors method test

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -105,6 +105,31 @@ RSpec.describe Park do
 
       expect(congress_park.all_attendees).to eq(["Charlie", "Honor", "Jim", "Kara", "Taylor"])
     end
+
+    it "Generates a list of all minors names, sorted alphabetically" do
+      congress_park = Park.new("Congress Park", 10)
+      vehicle1 = Vehicle.new("2001", "Honda", "Civic")
+      vehicle2 = Vehicle.new("2010", "Ford", "Taurus")
+      vehicle3 = Vehicle.new("2020", "Toyota", "Rav4")
+      vehicle4 = Vehicle.new("2020", "Toyota", "Rav4")
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})  
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+      kara = Passenger.new({"name" => "Kara", "age" => 30})
+      jim = Passenger.new({"name" => "Jim", "age" => 30})
+      honor = Passenger.new({"name" => "Honor", "age" => 17})
+
+      vehicle1.add_passenger(charlie)
+      vehicle2.add_passenger(taylor)
+      vehicle3.add_passenger(kara)
+      vehicle3.add_passenger(jim)
+      vehicle4.add_passenger(honor)
+      congress_park.add_vehicle(vehicle1)
+      congress_park.add_vehicle(vehicle2)
+      congress_park.add_vehicle(vehicle3)
+      congress_park.add_vehicle(vehicle4)
+
+      expect(congress_park.minors).to eq(["Honor", "Taylor"])
+    end
   end
 end
 
