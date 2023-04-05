ec23f8618f20ef570e026fe3dda271c65aaed7a6
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-05 10:08:32 -0600

    feat: Add to the add_vehicles method so passengers tests pass

--- lib/park.rb
+++ lib/park.rb
@@ -1,15 +1,20 @@
 class Park
   attr_reader :name, 
               :price, 
-              :vehicles
+              :vehicles,
+              :passengers
 
   def initialize(name, price)
     @name = name
     @price = price
     @vehicles = []
+    @passengers = []
   end
 
   def add_vehicle(vehicle)
     @vehicles << vehicle
+    @passengers += vehicle.passengers
   end
+
+  
 end
