06a48e9932190ceb2436e885ec3d3d10e883b58e
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-05 09:25:41 -0600

    feat: Add sped, speeding?, speed methods

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,9 +1,21 @@
 class Vehicle
-    attr_reader :year, :make, :model
+  attr_reader :year, :make, :model, :sped
   
-    def initialize(year, make, model)
+  def initialize(year, make, model)
     @year = year
     @make = make
     @model = model
+    @sped = false
   end
+
+  def speeding?
+    @sped
+  end
+
+  def speed
+    @sped = true
+  end
+
+
+
 end
